6410d6c69cc309ee543f560712b574045f0bba97
rmachnik <rmachnik@yahoo.pl>
2018-10-29 18:44:03 +0100

    Sizing down images, js, cs minimization, log4j.

--- src/main/java/pl.rmachnik/Portfolio.java
+++ src/main/java/pl.rmachnik/Portfolio.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import io.javalin.Javalin;
 import io.javalin.rendering.JavalinRenderer;
 import io.javalin.rendering.template.JavalinVelocity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -14,13 +16,14 @@ import java.util.List;
 import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class Portfolio {
+    static Logger LOG = LoggerFactory.getLogger(Portfolio.class);
+
     public static void main(String[] args) throws UnsupportedEncodingException {
         JavalinRenderer.register(JavalinVelocity.INSTANCE);
+        int port = Integer.valueOf(System.getProperty("port", "80"));
 
         Javalin app = Javalin.create();
         app.enableStaticFiles("web");
-
-        int port = Integer.valueOf(System.getProperty("port", "80"));
         app.start(port);
 
         InputStream resourceAsStream = Portfolio.class.getResourceAsStream("/portfolio.json");
