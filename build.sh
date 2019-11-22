79b16c01c15315f5be84b48e7e3ad15c76d52681
rmachnik <rmachnik@yahoo.pl>
2019-11-22 11:44:12 +0100

    Changing build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e
 echo "build parameter: $1 $2"
-export JAVA_HOME="/home/rmachnik/sdkman/candidates/java/12.0.2-open/bin"
+export JAVA_HOME="/home/rmachnik/sdkman/candidates/java/12.0.2-open"
 echo $JAVA_HOME
 mvn --version
 mvn releaser:release
