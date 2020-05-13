c42f8f0ab8484f698b4fb0c4d5ac9fd3c94cc5cb
rmachnik <rmachnik@yahoo.pl>
2020-05-13 22:12:16 +0200

    Fixing build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e
 echo "build parameter: $1 $2"
-export JAVA_HOME="/home/rmachnik/.sdkman/candidates/java/14.0.1-open"
+export JAVA_HOME="~/.sdkman/candidates/java/14.0.1-open"
 echo $JAVA_HOME
 mvn --version
 mvn releaser:release
